92b46ab324337a13aba28315b36fee3c04ad9c3b
MightBeKaz-YouNeverKnow <000972147@mohawkcollege.ca>
2025-12-05 11:29:38 -0500

    Fish now doubles points. also "points' is now spelled correctly

--- Assignment 5-Plinko/Assets/Fish Trigger.cs
+++ Assignment 5-Plinko/Assets/Fish Trigger.cs
@@ -6,11 +6,12 @@ public class FishTrigger : MonoBehaviour
 {
     
     public Player player;
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         
         player.FishMode();
-
+        
 
         //Destroy the object
         Destroy(collider.gameObject);
